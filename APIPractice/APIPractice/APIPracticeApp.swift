2dc3138e875dbfd2583cbd3286f0178ba1df9cab
Filippos96 <kaha21dg@student.ju.se>
2022-11-25 17:57:09 +0100

    2022-11-25

---
+++ APIPractice/APIPractice/APIPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  APIPracticeApp.swift
+//  APIPractice
+//
+//  Created by Harry Karl Filip Karlsson on 2022-11-25.
+//
+
+import SwiftUI
+
+@main
+struct APIPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ APIPractice/APIPractice/TaskData.swift
@@ -0,0 +1,14 @@
+//
+//  TaskData.swift
+//  APIPractice
+//
+//  Created by Harry Karl Filip Karlsson on 2022-11-25.
+//
+
+import Foundation
+
+struct TaskData{
+    let activity: String
+    let type: String
+    let key: Int
+}
